7e2bb626dd2c99180e4beeeb197fad5cba0e3d0f
Your Name <email@example.com>
2018-08-13 22:24:03 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# vueAdmin
-一个通用的前端后台管理UI基础包
+> 这个版本的vueAdmin ui是针对官网版本进行修改而成，主要修改了后端接口修改，登录等等，一个通用的前端工程基础包，即开即用,省去了前期繁琐配置
+
+### Demo
+![eee.gif](https://upload-images.jianshu.io/upload_images/9505682-d18a194d4960f547.gif?imageMogr2/auto-orient/strip)
+
+
+### Build Setup
+```
+# Clone project
+git clone https://github.com/1049759078/vueAdmin.git
+# Install dependencies
+npm install
+
+# 建议不要用cnpm  安装有各种诡异的bug 可以通过如下操作解决npm速度慢的问题
+npm install --registry=https://registry.npm.taobao.org
+
+# serve with hot reload at localhost:9528
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+## License
+
+The code is available under the [MIT license](https://opensource.org/licenses/MIT).
+
+![image](http://upload-images.jianshu.io/upload_images/9505682-938872aa3ee8b2c9.jpg?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240)
---
+++ src/utils/request.js
@@ -0,0 +1,52 @@
+import axios from 'axios'
+import { Message } from 'element-ui'
+import store from '../store'
+import { getToken } from '@/utils/auth'
+import router from '../router'
+// 创建axios实例
+const service = axios.create({
+  baseURL: process.env.BASE_API, // api的base_url
+  timeout: 5000 // 请求超时时间
+})
+
+// request拦截器
+service.interceptors.request.use(config => {
+  if (store.getters.token) {
+    config.headers['Authorization'] = 'JWT ' + getToken() // 让每个请求携带自定义token 请根据实际情况自行修改
+  }
+  return config
+}, error => {
+  // Do something with request error
+  console.log(error) // for debug
+  Promise.reject(error)
+})
+
+// respone拦截器
+service.interceptors.response.use(
+  response => {
+    return response.data
+  },
+  error => {
+    if (error.response.status === 401) {
+      store.dispatch('FedLogOut').then(() => {
+        router.push({ path: '/login' })
+        Message({
+          message: '登陆超时, 请重新登陆',
+          type: 'error',
+          duration: 5 * 1000
+        })
+      })
+    } else if (error.response.status === 403) {
+      Message({
+        message: '权限拒绝',
+        type: 'error'
+
+      })
+    }
+    console.log('err' + error)// for debug
+
+    return Promise.reject(error)
+  }
+)
+
+export default service
